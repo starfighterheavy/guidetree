eaa2664f86fbaf067b1cf5445734a04b1a4f4b08
Jonathan Kirst <jskirst@gmail.com>
2017-12-04 16:47:40 -0800

    Adding public-uuid based sharing link for trees (#10)
    Trees now have a Uuid that can be used in a shareable link to give unauthenticated access to tree.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :authenticate_user!, unless: :skip_authentication?
 
   private
+
+  def skip_authentication?
+    devise_controller?
+  end
 
   def set_flash_error(object)
     flash[:error] = object.errors.full_messages.join('.')
--- app/models/tree.rb
+++ app/models/tree.rb
@@ -4,6 +4,12 @@ class Tree < ActiveRecord::Base
   has_many :objectives
   belongs_to :user
 
+  validates :public_uuid, presence: true
+
+  before_validation do
+    self.public_uuid ||= SecureRandom::uuid()
+  end
+
   def roots
     objectives.roots
   end
